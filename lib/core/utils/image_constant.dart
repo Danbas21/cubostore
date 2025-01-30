bba292651fa3bc359e5b23b0b2bc01925b22ff0e
Danbas21 <149043410+Danbas21@users.noreply.github.com>
2025-01-30 00:13:04 -0600

    searcsssh

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,84 @@
+class ImageConstant {
+  static const String imagePath = 'assets/images';
+
+  // Previous constants from different documents
+  static const String imgLogo = '$imagePath/img_logo.png';
+  static const String imaRectangle1025 = '$imagePath/ima_rectangle_1025.png';
+  static const String imgCheckmarkWhiteA700 =
+      '$imagePath/img_checkmark_white_a700.png';
+  static const String imgSignal = '$imagePath/img_signal.svg';
+  static const String imgFavoritePrimary24x24 =
+      '$imagePath/img_favorite_primary_24x24.svg';
+  static const String imgImage5788x88 = '$imagePath/img_image_57_88x88.png';
+  static const String imgPlay = '$imagePath/img_play.png';
+  static const String imgImage571 = '$imagePath/img_image_57_1.png';
+  static const String imgGroup14 = '$imagePath/img_group_14.png';
+  static const String imageNotFound = 'assets/images/image_not_found.png';
+
+  // Navigation and arrow icons
+  static const String imgSettings = '$imagePath/img_settings.svg';
+  static const String imgImage325 = '$imagePath/img_image_325.png';
+  static const String imgGoogle = '$imagePath/img_google.svg';
+  static const String imgFacebook = '$imagePath/img_facebook.svg';
+  static const String imgApple = '$imagePath/img_apple.svg';
+  static const String img24pxArrowLeft = '$imagePath/img_24px_arrow_left.svg';
+  static const String imgCheckmark = '$imagePath/img_checkmark.svg';
+  static const String imgCall = '$imagePath/img_call.svg';
+  static const String imgCheck = '$imagePath/img_check.svg';
+  static const String imgRectangle854 = '$imagePath/img_rectangle_854.svg';
+  static const String imgGroup1000006039 =
+      '$imagePath/img_group_1000006039.svg';
+  static const String imgCamera = '$imagePath/img_camera.svg';
+  static const String imgRewind = '$imagePath/img_rewind.svg';
+  static const String imgSnapshop = '$imagePath/img_snapshop.svg';
+  static const String imgImage = '$imagePath/img_image.png';
+  static const String imgBag = '$imagePath/img_bag.svg';
+  static const String imgFavorite = '$imagePath/img_favorite.svg';
+  static const String imgImage216x162 = '$imagePath/img_image_216x162.png';
+  static const String imgImage1 = '$imagePath/img_image_1.png';
+  static const String imgImage2 = '$imagePath/img_image_2.png';
+
+  // Truck and shipping related images
+  static const String imgMapTruckLine = '$imagePath/img_map_truck_line.svg';
+  static const String imgMapTruckLineLightBlueA700 =
+      '$imagePath/img_map_truck_line_light_blue_a700.svg';
+  static const String imgCategoryShipping =
+      '$imagePath/img_category_shipping.svg';
+  static const String imgCategoryShippingPrimary =
+      '$imagePath/img_category_shipping_primary.svg';
+
+  // Check and verification icons
+  static const String imgSystemCheckLine =
+      '$imagePath/img_system_check_line.svg';
+  static const String imgCategoryCheckCircle =
+      '$imagePath/img_category_check_circle.svg';
+  static const String imgCheckGray50 = '$imagePath/img_check_gray_50.svg';
+  static const String imgCheckboxUncheckedPrimary =
+      '$imagePath/img_checkbox_unchecked_primary.svg';
+
+  // User and profile icons
+  static const String imgUserUserSmileLine =
+      '$imagePath/img_user_user_smile_line.svg';
+
+  // Gift and finance related
+  static const String imgFinanceGift2Line =
+      '$imagePath/img_finance_gift_2_line.svg';
+  static const String imgFinanceGift2LineAmberA200 =
+      '$imagePath/img_finance_gift_2_line_amber_a200.svg';
+
+  // Arrow icons
+  static const String imgArrowUp = '$imagePath/img_arrow_up.svg';
+  static const String imgArrowRightGray400 =
+      '$imagePath/img_arrow_right_gray_400.svg';
+  static const String imgArrowRightOnprimarycontainer =
+      '$imagePath/img_arrow_right_onprimarycontainer.svg';
+
+  // Category icons
+  static const String imgCategoryReceived =
+      '$imagePath/img_category_received.svg';
+  static const String imgCategoryCheckCircleOnprimarycontainer =
+      '$imagePath/img_category_check_circle_onprimarycontainer.svg';
+
+  // Frame and additional icons
+  static const String imgFrame = '$imagePath/img_frame.svg';
+}
